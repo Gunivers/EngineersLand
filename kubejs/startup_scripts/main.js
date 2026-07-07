0e78e102fba88fd1b645f62262a91e3db88ecfae
TretinV3 <tretinv3@laposte.net>
2026-07-07 18:05:18 +0200

    feat: add various recipes

---
+++ kubejs/startup_scripts/main.js
@@ -0,0 +1,33 @@
+// Visit the wiki for more info - https://kubejs.com/
+console.info('Hello, World! (Loaded startup example script)')
+
+StartupEvents.registry('block', event => {
+    event.create('cloud') // Create a new block
+        .displayName('Cloud') // Set a custom name
+        .soundType('wool') // Set a material (affects the sounds and some properties)
+        .hardness(0) // Set hardness (affects mining time)
+        .resistance(0) // Set resistance (to explosions, etc)
+        .opaque(false)
+        .renderType('translucent')
+        //.textureAll('kubejs:block/cloud')
+        .noCollision()
+        .notSolid()
+        .noDrops()
+        .slipperiness(0.5)
+        .noValidSpawns(true)
+        .viewBlocking(false)
+        .transparent(true)
+})
+
+StartupEvents.registry('item', event => {
+  event.create('inert_lapis').displayName("Inert Lapis Lazuli")
+  event.create('forming_lapis').displayName("Forming Lapis Lazuli")
+})
+
+BlockEvents.modification(event => {
+    event.modify('tfmg:oil_deposit', block => {
+        block.hasCollision = false;
+        block.destroySpeed = 0
+        block.explosionResistance = 0
+    })
+})
